a7f1f878d024f0c2fa59115e5c4f9cf8befc45ff
francesco settecase <francesco.settecase@community.unipa.it>
2025-12-01 11:10:25 +0100

    creation of code structure

---
+++ core/src/test/java/io/sesProject/firstTestClass.java
@@ -0,0 +1,11 @@
+package io.sesProject;
+
+import org.junit.jupiter.api.Test;
+
+
+public class firstTestClass {
+    @Test
+    public void firstTestMethod() {
+        System.out.println("This is a test method in firstTestClass.");
+    }
+}
